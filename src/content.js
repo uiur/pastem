488194f981cfe32dea44c827401909a43e9775e5
Kazato Sugimoto <uiureo@gmail.com>
2015-07-31 21:49:42 +0900

    Don't have to wait domready

--- src/content.js
+++ src/content.js
@@ -1,47 +1,44 @@
 'use strict'
 
 require('whatwg-fetch')
-var domready = require('domready')
 var endpoint = require('./lib/oembed-endpoint.js')
 
 if ((/github\.com/).test(window.location.hostname)) {
-  domready(function () {
-    document.addEventListener('paste', function (e) {
-      var element = e.target
+  document.addEventListener('paste', function (e) {
+    var element = e.target
 
-      function replace (text) {
-        var val = element.value
-        var replacedText = val.substr(0, element.selectionStart) + text + val.substr(element.selectionEnd, val.length)
-        element.value = replacedText
+    function replace (text) {
+      var val = element.value
+      var replacedText = val.substr(0, element.selectionStart) + text + val.substr(element.selectionEnd, val.length)
+      element.value = replacedText
+    }
+
+    if (e.target.classList.contains('comment-form-textarea')) {
+      var input = e.clipboardData.getData('text/plain')
+
+      var url = null
+
+      try {
+        url = new window.URL(input)
+      } catch(e) {
+        console.log(e)
+        return
       }
 
-      if (e.target.classList.contains('comment-form-textarea')) {
-        var input = e.clipboardData.getData('text/plain')
+      e.preventDefault()
 
-        var url = null
+      window.fetch(endpoint(url) + '?url=' + url)
+        .then(function (res) {
+          return res.json()
+        })
+        .then(function (json) {
+          var imageURL = json.type === 'photo' ? json.url : json.thumbnail_url
+          var title = json.title || json.provider_name || ''
 
-        try {
-          url = new window.URL(input)
-        } catch(e) {
-          console.log(e)
-          return
-        }
+          var markdown = '[![' + title + '](' + imageURL + ')](' + url + ')'
 
-        e.preventDefault()
-
-        window.fetch(endpoint(url) + '?url=' + url)
-          .then(function (res) {
-            return res.json()
-          })
-          .then(function (json) {
-            var imageURL = json.type === 'photo' ? json.url : json.thumbnail_url
-            var title = json.title || json.provider_name || ''
-
-            var markdown = '[![' + title + '](' + imageURL + ')](' + url + ')'
-
-            replace(markdown)
-          })
-      }
-    })
+          replace(markdown)
+        })
+    }
   })
 }
